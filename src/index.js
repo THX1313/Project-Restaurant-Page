75f531a0bec0abb724c6da2e8fbcec90b598c165
THX1313 <williamhess@gmail.com>
2023-12-01 14:35:15 -0800

    Basic page using JavaScript and webpack

--- src/index.js
+++ src/index.js
@@ -1,24 +1,47 @@
 // Create a bare-bones homepage for a restaurant. Include an image, headline, and some copy about how wonderful the restaurant is.
 
-const container = document.querySelector(#content);
+import './style.css';
+import Icon from './icon.png';
 
 
-const background = document.createElement("img", src = "images/background.png", alt = "Image of restaurant");
-
-const welcome = document.createElement(HTMLDivElement);
-
-welcome.innerText = "Welcome to Oceanview To-Go Deli & Bakery";
+const container = document.querySelector('#content');
 
 
-const tagline = document.createElement(HTMLDivElement);
+// const background = document.createElement("img", src = "images/background.png", alt = "Image of restaurant");
 
-tagline.innerText = "The Premier Bakery in Berkeley, CA";
+const welcome = document.createElement('div');
+welcome.classList.add("welcome");
+welcome.textContent = "Welcome to Bette's Oceanview Diner";
 
-const description = document.createElement(HTMLDivElement);
 
-description.textContent = "Oceanview To-Go Deli & Bakery is the go-to bakery and deli in Berkeley, CA. If you are in the mood for quality food, we have some delicious items for you. We are insistent on only using fresh and high-quality ingredients, and our bakery is the perfect place to spend time with your friends and family. We are known for our outstanding food and service, providing our guests with a dining experience they will never forget";
+const tagline = document.createElement('div');
+tagline.classList.add("tagline");
+tagline.textContent = "The Premier Diner in Berkeley, CA";
 
-container.appendChild(background);
+const description = document.createElement('div');
+description.classList.add("description");
+description.textContent = "Bette's Oceanview Diner is the best diner in Berkeley, CA. If you are in the mood for quality food, we have some delicious items for you. We are insistent on only using fresh and high-quality ingredients, and our bakery is the perfect place to spend time with your friends and family. We are known for our outstanding food and service, providing our guests with a dining experience they will never forget";
+
+
+
+function component() {
+    const element = document.createElement('div');
+
+   // Add the image to our existing div.
+  const myIcon = new Image();
+
+  myIcon.src = Icon;
+  
+  
+ 
+  element.appendChild(myIcon);
+  
+  element.classList.add("icon");
+ 
+    return element;
+  }
+ 
+container.appendChild(component());
 container.appendChild(welcome);
 container.appendChild(tagline);
 container.appendChild(description);
